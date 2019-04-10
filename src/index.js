d7493a13725d930d649c1c43f3baf5089afb29c5
ITLackey <lackey_i@wustl.edu>
2019-04-10 16:37:34 -0500

    testing webpack

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { ComponentConfiguration } from "./infrastructure/ComponentConfiguration.js";
+import ComponentManager  from "./infrastructure/ComponentManager.js";
+import { TemplateFromUrlComponentLoader } from "./infrastructure/TemplateFromUrlComponentLoader.js";
