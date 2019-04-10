12e1209c4810bcfa6f804adaa714d2ba242470d4
ITLackey <lackey_i@wustl.edu>
2019-04-10 11:22:21 -0500

    setting up build

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,36 @@
-const {src,dest} = require('gulp');
-function compile(cb) {
-    console.log('compile');
-    return src('src/**/*.*').pipe(dest('sandbox'));
+const {src,dest, series} = require('gulp');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const path = require('path');
+const spawn = require('child_process').spawn;
+const outputPath = 'sandbox';
+
+function clean() {
+  return spawn('rm', ['-rf', path.join(__dirname, outputPath)]);
+}
+function minify(){
+    return src(path.join(outputPath, 'one-inch-punch.js'))
+    .pipe(rename('one-inch-punch.min.js'))    
+    .pipe(sourcemaps.init())
+        .pipe(uglify())
+        
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest(outputPath));
+}
+function compile(cb) {    
+    return src('src/**/*.js')
+    .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        //.pipe(uglify())
+        //.pipe(minify())
+        .pipe(concat('one-inch-punch.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest(outputPath));
 }
 
-exports.default = compile;
+exports.default =  series(clean, compile, minify);
